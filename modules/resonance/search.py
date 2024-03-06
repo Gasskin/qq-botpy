9b12e5d8f5a47baa5c13e9ca711defd3017e9515
Gasskin <415753928@qq.com>
2024-03-06 17:38:30 +0800

    123

--- modules/resonance/search.py
+++ modules/resonance/search.py
@@ -6,6 +6,7 @@ from botpy.message import Message
 
 import modules.resonance.configs.city_id as city_id
 import modules.resonance.configs.items_buy as items_buy
+import modules.resonance.configs.items_sell as items_sell
 
 _log = logging.get_logger()
 
@@ -16,31 +17,66 @@ async def Search(self, message: Message):
     return
 
 
+# 获取道具的ID
+def GetItemId(param: str) -> int:
+    id = 0
+    try:
+        id = int(param)
+    except:
+        id = 0
+        for item_id in items_buy.Datas:
+            item = items_buy.Datas[item_id]
+            if item["name"] == param:
+                id = item_id
+                break
+    return id
+
+
+# 获取城市信息
+def GetCityInfo(id: int):
+    for key_name in city_id.Datas:
+        city = city_id.Datas[key_name]
+        if city["id"] == id:
+            return city
+    return None
+
+
 # 查询商品基础信息
 async def SearchBasic(self, message: Message):
     params = message.content.split(" ")
+    # 查询基础购入价格
     if len(params) == 3:
-        try:
-            id = int(params[2])
-        except:
-            id = params[2]
-            for item_id in items_buy.Datas:
-                item = items_buy.Datas[item_id]
-                if item["name"] == params[2]:
-                    id = item_id
-                    break
+        id = GetItemId(params[2])
         if isinstance(id, int) and items_buy.Datas[id]:
             item = items_buy.Datas[id]
             city_name = "NULL"
-            for key_name in city_id.Datas:
-                city = city_id.Datas[key_name]
-                if city["id"] == item["city"]:
-                    city_name = city["name"]
-                    break
+            city = GetCityInfo(item["city"])
+            if city:
+                city_name = city["name"]
             await message.reply(
-                content=f"查询道具：{item['name']}\n所属地点：{city_name}\n基础数量：{item['num']}\n基础价格：{item['price']}"
+                content=f"查询道具：{item['name']}\n所属地点：{city_name}\n基础数量：{item['num']}\n基础购入价格：{item['price']}"
             )
         else:
             _log.info(f"error:不存在道具 {id}")
+    # 查询在所有城市的售价
+    elif len(params) == 4 and params[3] == "all":
+        id = GetItemId(params[2])
+        if items_buy.Datas[id] and items_sell[id]:
+            content = (
+                f"查询道具：{items_buy.Datas[id]['name']}\n所属地点：{city_name}\n"
+            )
+            for city_id in items_sell[id]:
+                city_name = "NULL"
+                city = GetCityInfo(city_id)
+                if city:
+                    city_name = city["name"]
+                content = (
+                    content
+                    + f"售出城市：{city_name} 基础售价：{items_sell[id][city_id]}"
+                )
+                return
+
+            await message.reply(content)
+        return
     else:
         _log.info("error:参数数量不匹配")
