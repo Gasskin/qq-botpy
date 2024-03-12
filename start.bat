3975ca3325e296eb93be98ab41acae135ab0dbf1
Gasskin <415753928@qq.com>
2024-03-12 18:06:23 +0800

    d

--- start.bat
+++ start.bat
@@ -1,10 +1,16 @@
 @echo off
 cd /d %~dp0
+
+
+
 git pull
+
 if %ERRORLEVEL% neq 0 (
     echo Git update failed, start.py will not be run.
     exit /b %ERRORLEVEL%
 )
 
+
+taskkill /f /im python.exe
 del /f /q botpy.log
 python start.py
